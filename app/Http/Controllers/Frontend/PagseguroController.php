4dcea9c7a84bf4368a055ef4e55a02479f029cb7
Gilson Melo <junnyorr.sirnandes@gmail.com>
2017-11-13 23:04:18 -0200

    atualizações

--- app/Http/Controllers/Frontend/PagseguroController.php
+++ app/Http/Controllers/Frontend/PagseguroController.php
@@ -38,6 +38,10 @@ class PagseguroController extends Controller
             $transaction->operational_fee_amount = isset($dataXml->operationalFeeAmount) ? $dataXml->operationalFeeAmount : null;
             $transaction->intermediation_fee_amount = isset($dataXml->creditorFees->intermediationFeeAmount) ? $dataXml->creditorFees->intermediationFeeAmount : null;
             $transaction->intermediation_fee_rate = isset($dataXml->intermediationRateAmount) ? $dataXml->intermediationFeeAmount : null;
+            var_dump($$dataXml->escrowEndDate);
+            $datetz = new Datetime($dataXml->escrowEndDate);
+
+            dd($datetz);
 
             $carbon = Carbon::instance($dataXml->escrowEndDate);                        // 'Carbon\Carbon'
             echo $carbon->toDateTimeString();
